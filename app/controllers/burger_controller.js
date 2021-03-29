0d4c075518b6fa936bc9189ef6d58dda664bb8c6
KevinFloris20 <75303163+KevinFloris20@users.noreply.github.com>
2021-03-29 18:02:51 -0400

    routers finished

--- app/controllers/burger_controller.js
+++ app/controllers/burger_controller.js
@@ -1,2 +1,19 @@
-const express = require("express");
-const burger = require("../controllers/burger_controller.js");
+const router = require("express").Router();
+const burger = require("../models/burger.js");
+
+router.get("/api/burgers", (rec, res)=>{
+    const burgers = burger.selectAll();
+    res.json(burgers);
+});
+
+router.post("/api/burgers", (rec,res)=>{
+    const burger = burger.insertOne(col, rec.body, function(){
+        res.json(burger)
+    });
+});
+
+router.put("/api/burgers/:id", (rec,res)=>{
+    const burger = burger.updateOne(col, rec.body, function(){
+        res.json(burger)
+    });
+});
